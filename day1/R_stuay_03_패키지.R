f06b0fb3e9247db352c570627be3a047df6f772f
lawlowlight <marhebasd@naver.com>
2022-05-11 16:16:11 +0900

    R 학습 첫날

---
+++ day1/R_stuay_03_패키지.R
@@ -0,0 +1,120 @@
+## 패키지 설치 및 로드 
+'Hello world'
+"Hello world!"
+install.packages('ggplot2')
+library(ggplot2)
+library(g.date)
+
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) + 
+  geom_point()
+
+## cowsay 패키지
+install.packages(('cowsay'))
+library(cowsay)
+
+say(what = 'Hi~', by = 'bat')
+
+# 시스템 함수
+Sys.Date()
+Sys.time()
+
+## 생일 맞추기 -==? ((m*4)+9)*25 + d
+(m * 100) + 225 + d
+900 + 225 + 21
+(1146 - 225)/100
+(1142 - 225)/100
+(931 = 225)/100
+# 소윤 주연 민철 석준 현석
+# 931  754  1029 1139 1442
+val = 391
+d <- (val - 225)%%100
+m <- ((val = 225) - (val = 225%%100))100
+cat(m, '월',d, '일')
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
